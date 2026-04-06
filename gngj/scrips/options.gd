7d886b013dac1b22b1ed70f01a0b0f175e569934
jaskress33 <greenmegaman33@gmail.com>
2026-04-06 16:19:30 -0500

    Options Menu Elevator Update
    The options menu for the most part is finished. Only thing missing would be the logic for a few parts of it as they aren't ready to be implemented (Exit, Main Menu, SFX volume,  Music Volume)

--- gngj/scrips/options.gd
+++ gngj/scrips/options.gd
@@ -6,14 +6,10 @@ func _ready() -> void:
 	$TabContainer/Sound/MasterVolumeSlider.value = SettingsManager.settings.master_volume
 	$TabContainer/Sound/MasterVolumeSlider/Label.text = "%d%%" % int(round(SettingsManager.settings.master_volume * 100))
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	$TabContainer/Sound/MasterVolumeSlider/Label.text = "%d%%" % int(round(SettingsManager.settings.master_volume * 100))
-
 #Change master volume
 func _on_master_value_changed(value: float):
 	SettingsManager.settings.master_volume = value
+	$TabContainer/Sound/MasterVolumeSlider/Label.text = "%d%%" % int(round(SettingsManager.settings.master_volume * 100))
 	SettingsManager.apply_all() 
 	SettingsManager.save_settings()
 
@@ -33,3 +29,13 @@ func _on_resolutions_item_selected(index: int):
 			SettingsManager.settings.resolution = Vector2i(1280,720)
 			SettingsManager.apply_all() 
 			SettingsManager.save_settings() 
+
+
+func _on_full_screen_toggled(toggled_on: bool) -> void:
+	SettingsManager.settings.fullscreen = toggled_on 
+	SettingsManager.apply_all()
+	SettingsManager.save_settings()
+
+
+func _on_master_mute_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), toggled_on)
