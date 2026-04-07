2407944837c631b423dadc6b03dc61a9e3e6f4ec
Fuzzy <luispelayo314@gmail.com>
2026-04-07 01:24:47 -0500

    have most of UI Button working. Had to delete credits screen

---
+++ gngj/scrips/back_room.gd
@@ -0,0 +1,45 @@
+extends Control
+
+@onready var ingredientsShelf : Button = $ingredientShelfBtn
+@onready var bookShelf: Button = $BookshelfBtn
+@onready var recipeBook : Button = $RecipeBookBtn
+@onready var cauldonStation : Button = $CauldronBtn
+@onready var grindStation : Button = $MortarandPestleBtn
+@onready var front : Button = $toFrontRoomBtn
+
+signal toIngredientsShelf
+signal toBookShelf
+signal toRecipeBook
+signal toCauldronStation
+signal toGrindStation
+signal toFront
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	ingredientsShelf.pressed.connect(_on_backRoomButton_pressed.bind("i"))
+	bookShelf.pressed.connect(_on_backRoomButton_pressed.bind("b"))
+	recipeBook.pressed.connect(_on_backRoomButton_pressed.bind("r"))
+	cauldonStation.pressed.connect(_on_backRoomButton_pressed.bind("c"))
+	grindStation.pressed.connect(_on_backRoomButton_pressed.bind("g"))
+	front.pressed.connect(_on_backRoomButton_pressed.bind("f"))
+	
+func _on_backRoomButton_pressed(name: String) -> void:
+	match name:
+		"i":
+			toIngredientsShelf.emit()
+		"b":
+			toBookShelf.emit()
+		"r":
+			toRecipeBook.emit()
+		"c":
+			toCauldronStation.emit()
+		"g":
+			toGrindStation.emit()
+		"f":
+			toFront.emit()
+	
+	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- gngj/scrips/ui.gd
+++ gngj/scrips/ui.gd
@@ -49,3 +49,35 @@ func _on_button_pressed(button_pressed: String) -> void:
 		pass
 	if (button_pressed == "toFrontRoom"):
 		pass
+
+
+func _on_main_menu_scene_start_game() -> void:
+	$MainMenuScene.hide()
+	$FrontRoom.show()
+
+func _on_front_room_to_back_room() -> void:
+	$FrontRoom.hide()
+	$BackRoom.show()
+
+func _on_back_room_to_front() -> void:
+	$BackRoom.hide()
+	$FrontRoom.show()
+
+func _on_back_room_to_grind_station() -> void:
+	$BackRoom.hide()
+	$GrindingStation.show()
+
+
+func _on_back_room_to_cauldron_station() -> void:
+	$BackRoom.hide()
+	$CauldronStation.show()
+
+
+func _on_grinding_station_go_back() -> void:
+	$GrindingStation.hide()
+	$BackRoom.show()
+
+
+func _on_cauldron_station_go_back() -> void:
+	$CauldronStation.hide()
+	$BackRoom.show()
