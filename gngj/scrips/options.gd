7c7cf76a6837077b0f73aaeaceb9acc5e73931bc
jaskress33 <greenmegaman33@gmail.com>
2026-04-09 19:32:28 -0500

    Sound sliders, options, resolution
    Sounds sliders work as intended, resized options menu to appear correctly, resolution is now only selectable when windowed mode is selected from options. Mute buttons are buggy besides master.

--- gngj/scrips/options.gd
+++ gngj/scrips/options.gd
@@ -6,19 +6,48 @@ signal exitOptions
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	exitButton.pressed.connect(_on_exitButton_pressed)
-	
+	$TabContainer/Display/Resolutions.disabled = !SettingsManager.settings.windowed
 	$TabContainer/Sound/MasterVolumeSlider.value = SettingsManager.settings.master_volume
-	$TabContainer/Sound/MasterVolumeSlider/Label.text = "%d%%" % int(round(SettingsManager.settings.master_volume * 100))
+	$TabContainer/Sound/MasterVolumeSlider/MasterLabel.text = "%d%%" % int(round(SettingsManager.settings.master_volume * 100))
+	$TabContainer/Sound/MusicVolumeSlider.value = SettingsManager.settings.music_volume
+	$TabContainer/Sound/MusicVolumeSlider/MusicLabel.text = "%d%%" % int(round(SettingsManager.settings.music_volume * 100))
+	$TabContainer/Sound/SFXVolumeSlider.value = SettingsManager.settings.sfx_volume
+	$TabContainer/Sound/SFXVolumeSlider/Label.text = "%d%%" % int(round(SettingsManager.settings.sfx_volume * 100))
 
 func _on_exitButton_pressed() -> void: 
 	exitOptions.emit()
 
 #Change master volume
 func _on_master_value_changed(value: float):
+	$TabContainer/Sound/MasterVolumeSlider/MasterLabel.text = "%d%%" % int(round(SettingsManager.settings.master_volume * 100))
 	SettingsManager.settings.master_volume = value
-	$TabContainer/Sound/MasterVolumeSlider/Label.text = "%d%%" % int(round(SettingsManager.settings.master_volume * 100))
 	SettingsManager.apply_all() 
 	SettingsManager.save_settings()
+	
+#Change music volume
+func _on_music_value_changed(value: float):
+	$TabContainer/Sound/MusicVolumeSlider/MusicLabel.text = "%d%%" % int(round(SettingsManager.settings.music_volume * 100))
+	SettingsManager.settings.music_volume = value
+	SettingsManager.apply_all()
+	SettingsManager.save_settings()
+	
+#Change sfx volume	
+func _on_sfx_volume_value_changed(value: float) -> void:
+	$TabContainer/Sound/SFXVolumeSlider/Label.text = "%d%%" % int(round(SettingsManager.settings.sfx_volume * 100)) 
+	SettingsManager.settings.sfx_volume = value
+	SettingsManager.apply_all() 
+	SettingsManager.save_settings()
+
+#Mute master volume
+func _on_master_mute_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), toggled_on)
+#Mute music volume
+func _on_music_mute_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), toggled_on)
+#Mute sfx volume
+func _on_sfx_mute_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("SFX"), toggled_on)
+
 
 #Change resolution of the game
 func _on_resolutions_item_selected(index: int):
@@ -37,12 +66,8 @@ func _on_resolutions_item_selected(index: int):
 			SettingsManager.apply_all() 
 			SettingsManager.save_settings() 
 
-
 func _on_full_screen_toggled(toggled_on: bool) -> void:
-	SettingsManager.settings.fullscreen = toggled_on 
+	SettingsManager.settings.windowed = toggled_on 
+	$TabContainer/Display/Resolutions.disabled = !SettingsManager.settings.windowed
 	SettingsManager.apply_all()
 	SettingsManager.save_settings()
-
-
-func _on_master_mute_toggled(toggled_on: bool) -> void:
-	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), toggled_on)
