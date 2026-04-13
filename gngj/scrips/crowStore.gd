02832e43566a92afaaf158d4949d2a489d2a97a1
Fuzzy <luispelayo314@gmail.com>
2026-04-12 20:13:24 -0500

    crow store is workin

--- gngj/scrips/crowStore.gd
+++ gngj/scrips/crowStore.gd
@@ -2,16 +2,111 @@ extends Control
 
 @onready var continueButton : Button = $continueButton
 
-
-
+@onready var shopOption1 : TextureButton = $allButtons/shopOption1
+@onready var shopOption2 : TextureButton = $allButtons/shopOption2
+@onready var shopOption3 : TextureButton = $allButtons/shopOption3
+@onready var shopOption4 : TextureButton = $allButtons/shopOption4
+@onready var shopOption5 : TextureButton = $allButtons/shopOption5
+@onready var shopOption6 : TextureButton = $allButtons/shopOption6
+@onready var shopOption7 : TextureButton = $allButtons/shopOption7
+@onready var shopOption8 : TextureButton = $allButtons/shopOption8
+@onready var shopOption9 : TextureButton = $allButtons/shopOption9
+@onready var shopOption10 : TextureButton = $allButtons/shopOption10
+@onready var shopOption11 : TextureButton = $allButtons/shopOption11
+@onready var shopOption12 : TextureButton = $allButtons/shopOption12
+@onready var shopOption13 : TextureButton = $allButtons/shopOption13
+@onready var shopOption14 : TextureButton = $allButtons/shopOption14
+@onready var shopOption15 : TextureButton = $allButtons/shopOption15
+@onready var shopOption16 : TextureButton = $allButtons/shopOption16
 
 signal newDay
-signal buy
+signal buy(cost, item)
+
+var currency
+var redColor = Color(0.926, 0.0, 0.0, 1.0)
+var blackColor = Color()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	continueButton.pressed.connect(_on_continueButton_pressed)
-
+	
+	shopOption1.mouse_entered.connect(_on_mouse_entered.bind(shopOption1))
+	shopOption1.mouse_exited.connect(_on_mouse_exited.bind(shopOption1))
+	shopOption1.pressed.connect(_on_buyIngredient.bind("air's brevity"))
+	
+	shopOption2.mouse_entered.connect(_on_mouse_entered.bind(shopOption2))
+	shopOption2.mouse_exited.connect(_on_mouse_exited.bind(shopOption2))
+	shopOption2.pressed.connect(_on_buyIngredient.bind("desert calm"))
+	
+	
+	shopOption3.mouse_entered.connect(_on_mouse_entered.bind(shopOption3))
+	shopOption3.mouse_exited.connect(_on_mouse_exited.bind(shopOption3))
+	shopOption3.pressed.connect(_on_buyIngredient.bind("dried ocean"))
+	
+	
+	shopOption4.mouse_entered.connect(_on_mouse_entered.bind(shopOption4))
+	shopOption4.mouse_exited.connect(_on_mouse_exited.bind(shopOption4))
+	shopOption4.pressed.connect(_on_buyIngredient.bind("ear of the sea"))
+	
+	
+	shopOption5.mouse_entered.connect(_on_mouse_entered.bind(shopOption5))
+	shopOption5.mouse_exited.connect(_on_mouse_exited.bind(shopOption5))
+	shopOption5.pressed.connect(_on_buyIngredient.bind("gaia's teeth"))
+	
+	
+	shopOption6.mouse_entered.connect(_on_mouse_entered.bind(shopOption6))
+	shopOption6.mouse_exited.connect(_on_mouse_exited.bind(shopOption6))
+	shopOption6.pressed.connect(_on_buyIngredient.bind("dragon's egg"))
+	
+	
+	shopOption7.mouse_entered.connect(_on_mouse_entered.bind(shopOption7))
+	shopOption7.mouse_exited.connect(_on_mouse_exited.bind(shopOption7))
+	shopOption7.pressed.connect(_on_buyIngredient.bind("spring crystal"))
+	
+	
+	shopOption8.mouse_entered.connect(_on_mouse_entered.bind(shopOption8))
+	shopOption8.mouse_exited.connect(_on_mouse_exited.bind(shopOption8))
+	shopOption8.pressed.connect(_on_buyIngredient.bind("string of lovers"))
+	
+	
+	shopOption9.mouse_entered.connect(_on_mouse_entered.bind(shopOption9))
+	shopOption9.mouse_exited.connect(_on_mouse_exited.bind(shopOption9))
+	shopOption9.pressed.connect(_on_buyIngredient.bind("venus's eyelashes"))
+	
+	
+	shopOption10.mouse_entered.connect(_on_mouse_entered.bind(shopOption10))
+	shopOption10.mouse_exited.connect(_on_mouse_exited.bind(shopOption10))
+	shopOption10.pressed.connect(_on_buyIngredient.bind("winter crystal"))
+	
+	
+	shopOption11.mouse_entered.connect(_on_mouse_entered.bind(shopOption11))
+	shopOption11.mouse_exited.connect(_on_mouse_exited.bind(shopOption11))
+	shopOption11.pressed.connect(_on_buyIngredient.bind("psychic stone"))
+	
+	
+	shopOption12.mouse_entered.connect(_on_mouse_entered.bind(shopOption12))
+	shopOption12.mouse_exited.connect(_on_mouse_exited.bind(shopOption12))
+	shopOption12.pressed.connect(_on_buyIngredient.bind("shattered sky"))
+	
+	
+	shopOption13.mouse_entered.connect(_on_mouse_entered.bind(shopOption13))
+	shopOption13.mouse_exited.connect(_on_mouse_exited.bind(shopOption13))
+	shopOption13.pressed.connect(_on_buyIngredient.bind("shooting star"))
+	
+	
+	shopOption14.mouse_entered.connect(_on_mouse_entered.bind(shopOption14))
+	shopOption14.mouse_exited.connect(_on_mouse_exited.bind(shopOption14))
+	shopOption14.pressed.connect(_on_buyIngredient.bind("a leaf of a thousand leaves"))
+	
+	
+	shopOption15.mouse_entered.connect(_on_mouse_entered.bind(shopOption15))
+	shopOption15.mouse_exited.connect(_on_mouse_exited.bind(shopOption15))
+	shopOption15.pressed.connect(_on_buyIngredient.bind("a thorny heart"))
+	
+	shopOption16.mouse_entered.connect(_on_mouse_entered.bind(shopOption16))
+	shopOption16.mouse_exited.connect(_on_mouse_exited.bind(shopOption16))
+	shopOption16.pressed.connect(_on_buyIngredient.bind("tears of trees"))
+	
 	
 func _on_continueButton_pressed() -> void:
 	newDay.emit()
@@ -22,4 +117,52 @@ func _on_buyIngredient(ingredient : String):
 			buy.emit(2,int($allButtons/shopOption1/optionCost.text))
 		"aspestos":
 			buy.emit(0,int($allButtons/shopOption2/optionCost.text))
-			
+		"a leaf of a thousand leaves":
+			buy.emit(1, ingredient)
+		"a thorny heart":
+			buy.emit(1, ingredient)
+		"tears of trees":
+			buy.emit(1, ingredient)
+		"air's brevity":
+			buy.emit(2, ingredient)
+		"desert calm":
+			buy.emit(2, ingredient)
+		"dried ocean":
+			buy.emit(2, ingredient)
+		"ear of the sea":
+			buy.emit(2, ingredient)
+		"gaia's teeth":
+			buy.emit(2, ingredient)
+		"dragon's egg":
+			buy.emit(3, ingredient)
+		"spring crystal":
+			buy.emit(3, ingredient)
+		"string of lovers":
+			buy.emit(3, ingredient)
+		"venus's eyelashes":
+			buy.emit(3, ingredient)
+		"winter crystal":
+			buy.emit(3, ingredient)
+		"psychic stone":
+			buy.emit(4, ingredient)
+		"shattered sky":
+			buy.emit(5, ingredient)
+		"shooting star":
+			buy.emit(6, ingredient)
+				
+	checkIfTooBroke()
+
+func checkIfTooBroke():
+	for button in $allButtons.get_children():
+		var lable = button.get_child(0)
+		if(int(lable.text) > currency):
+			lable.set("theme_override_colors/font_color", redColor)
+		else:
+			lable.set("theme_override_colors/font_color", blackColor)
+
+func _on_mouse_entered(button: Variant):
+	button.material.set_shader_parameter("outline_thickness", 5.0)
+
+
+func _on_mouse_exited(button: Variant):
+	button.material.set_shader_parameter("outline_thickness", 0.0)
