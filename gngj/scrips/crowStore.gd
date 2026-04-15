63370271abef439b6cf0185c440e06a7b89f4ddc
Carter-Layton <carter.layt@gmail.com>
2026-04-14 23:52:03 -0500

    more ui stuff
    most importantly added a new label for hovering over stuff

--- gngj/scrips/crowStore.gd
+++ gngj/scrips/crowStore.gd
@@ -1,5 +1,9 @@
 extends Control
 
+var spawn_test = preload("res://Scenes/Spawn_Test.tscn")
+var allIngredients
+var instance
+
 @onready var continueButton : Button = $continueButton
 
 @onready var shopOption1 : TextureButton = $allButtons/shopOption1
@@ -30,86 +34,88 @@ var blackColor = Color()
 func _ready() -> void:
 	continueButton.pressed.connect(_on_continueButton_pressed)
 	
-	shopOption1.mouse_entered.connect(_on_mouse_entered.bind(shopOption1))
-	shopOption1.mouse_exited.connect(_on_mouse_exited.bind(shopOption1))
+	shopOption1.mouse_entered.connect(_on_mouse_entered.bind(shopOption1, "air's brevity"))
+	shopOption1.mouse_exited.connect(_on_mouse_exited.bind(shopOption1, "air's brevity"))
 	shopOption1.pressed.connect(_on_buyIngredient.bind("air's brevity"))
 	
-	shopOption2.mouse_entered.connect(_on_mouse_entered.bind(shopOption2))
-	shopOption2.mouse_exited.connect(_on_mouse_exited.bind(shopOption2))
+	shopOption2.mouse_entered.connect(_on_mouse_entered.bind(shopOption2, "desert calm"))
+	shopOption2.mouse_exited.connect(_on_mouse_exited.bind(shopOption2, "desert calm"))
 	shopOption2.pressed.connect(_on_buyIngredient.bind("desert calm"))
 	
 	
-	shopOption3.mouse_entered.connect(_on_mouse_entered.bind(shopOption3))
-	shopOption3.mouse_exited.connect(_on_mouse_exited.bind(shopOption3))
+	shopOption3.mouse_entered.connect(_on_mouse_entered.bind(shopOption3, "dried ocean"))
+	shopOption3.mouse_exited.connect(_on_mouse_exited.bind(shopOption3, "dried ocean"))
 	shopOption3.pressed.connect(_on_buyIngredient.bind("dried ocean"))
 	
 	
-	shopOption4.mouse_entered.connect(_on_mouse_entered.bind(shopOption4))
-	shopOption4.mouse_exited.connect(_on_mouse_exited.bind(shopOption4))
+	shopOption4.mouse_entered.connect(_on_mouse_entered.bind(shopOption4, "ear of the sea"))
+	shopOption4.mouse_exited.connect(_on_mouse_exited.bind(shopOption4, "ear of the sea"))
 	shopOption4.pressed.connect(_on_buyIngredient.bind("ear of the sea"))
 	
 	
-	shopOption5.mouse_entered.connect(_on_mouse_entered.bind(shopOption5))
-	shopOption5.mouse_exited.connect(_on_mouse_exited.bind(shopOption5))
+	shopOption5.mouse_entered.connect(_on_mouse_entered.bind(shopOption5, "gaia's teeth"))
+	shopOption5.mouse_exited.connect(_on_mouse_exited.bind(shopOption5, "gaia's teeth"))
 	shopOption5.pressed.connect(_on_buyIngredient.bind("gaia's teeth"))
 	
 	
-	shopOption6.mouse_entered.connect(_on_mouse_entered.bind(shopOption6))
-	shopOption6.mouse_exited.connect(_on_mouse_exited.bind(shopOption6))
+	shopOption6.mouse_entered.connect(_on_mouse_entered.bind(shopOption6, "dragon's egg"))
+	shopOption6.mouse_exited.connect(_on_mouse_exited.bind(shopOption6, "dragon's egg"))
 	shopOption6.pressed.connect(_on_buyIngredient.bind("dragon's egg"))
 	
 	
-	shopOption7.mouse_entered.connect(_on_mouse_entered.bind(shopOption7))
-	shopOption7.mouse_exited.connect(_on_mouse_exited.bind(shopOption7))
+	shopOption7.mouse_entered.connect(_on_mouse_entered.bind(shopOption7, "spring crystal"))
+	shopOption7.mouse_exited.connect(_on_mouse_exited.bind(shopOption7, "spring crystal"))
 	shopOption7.pressed.connect(_on_buyIngredient.bind("spring crystal"))
 	
 	
-	shopOption8.mouse_entered.connect(_on_mouse_entered.bind(shopOption8))
-	shopOption8.mouse_exited.connect(_on_mouse_exited.bind(shopOption8))
+	shopOption8.mouse_entered.connect(_on_mouse_entered.bind(shopOption8, "string of lovers"))
+	shopOption8.mouse_exited.connect(_on_mouse_exited.bind(shopOption8, "string of lovers"))
 	shopOption8.pressed.connect(_on_buyIngredient.bind("string of lovers"))
 	
 	
-	shopOption9.mouse_entered.connect(_on_mouse_entered.bind(shopOption9))
-	shopOption9.mouse_exited.connect(_on_mouse_exited.bind(shopOption9))
+	shopOption9.mouse_entered.connect(_on_mouse_entered.bind(shopOption9, "venus's eyelashes"))
+	shopOption9.mouse_exited.connect(_on_mouse_exited.bind(shopOption9, "venus's eyelashes"))
 	shopOption9.pressed.connect(_on_buyIngredient.bind("venus's eyelashes"))
 	
 	
-	shopOption10.mouse_entered.connect(_on_mouse_entered.bind(shopOption10))
-	shopOption10.mouse_exited.connect(_on_mouse_exited.bind(shopOption10))
+	shopOption10.mouse_entered.connect(_on_mouse_entered.bind(shopOption10, "winter crystal"))
+	shopOption10.mouse_exited.connect(_on_mouse_exited.bind(shopOption10, "winter crystal"))
 	shopOption10.pressed.connect(_on_buyIngredient.bind("winter crystal"))
 	
 	
-	shopOption11.mouse_entered.connect(_on_mouse_entered.bind(shopOption11))
-	shopOption11.mouse_exited.connect(_on_mouse_exited.bind(shopOption11))
+	shopOption11.mouse_entered.connect(_on_mouse_entered.bind(shopOption11, "psychic stone"))
+	shopOption11.mouse_exited.connect(_on_mouse_exited.bind(shopOption11, "psychic stone"))
 	shopOption11.pressed.connect(_on_buyIngredient.bind("psychic stone"))
 	
 	
-	shopOption12.mouse_entered.connect(_on_mouse_entered.bind(shopOption12))
-	shopOption12.mouse_exited.connect(_on_mouse_exited.bind(shopOption12))
+	shopOption12.mouse_entered.connect(_on_mouse_entered.bind(shopOption12, "shattered sky"))
+	shopOption12.mouse_exited.connect(_on_mouse_exited.bind(shopOption12, "shattered sky"))
 	shopOption12.pressed.connect(_on_buyIngredient.bind("shattered sky"))
 	
 	
-	shopOption13.mouse_entered.connect(_on_mouse_entered.bind(shopOption13))
-	shopOption13.mouse_exited.connect(_on_mouse_exited.bind(shopOption13))
+	shopOption13.mouse_entered.connect(_on_mouse_entered.bind(shopOption13, "shooting star"))
+	shopOption13.mouse_exited.connect(_on_mouse_exited.bind(shopOption13, "shooting star"))
 	shopOption13.pressed.connect(_on_buyIngredient.bind("shooting star"))
 	
 	
-	shopOption14.mouse_entered.connect(_on_mouse_entered.bind(shopOption14))
-	shopOption14.mouse_exited.connect(_on_mouse_exited.bind(shopOption14))
+	shopOption14.mouse_entered.connect(_on_mouse_entered.bind(shopOption14, "leaf of a thousand leaves"))
+	shopOption14.mouse_exited.connect(_on_mouse_exited.bind(shopOption14, "leaf of a thousand leaves"))
 	shopOption14.pressed.connect(_on_buyIngredient.bind("leaf of a thousand leaves"))
 	
 	
-	shopOption15.mouse_entered.connect(_on_mouse_entered.bind(shopOption15))
-	shopOption15.mouse_exited.connect(_on_mouse_exited.bind(shopOption15))
+	shopOption15.mouse_entered.connect(_on_mouse_entered.bind(shopOption15, "a thorny heart"))
+	shopOption15.mouse_exited.connect(_on_mouse_exited.bind(shopOption15, "a thorny heart"))
 	shopOption15.pressed.connect(_on_buyIngredient.bind("a thorny heart"))
 	
-	shopOption16.mouse_entered.connect(_on_mouse_entered.bind(shopOption16))
-	shopOption16.mouse_exited.connect(_on_mouse_exited.bind(shopOption16))
+	shopOption16.mouse_entered.connect(_on_mouse_entered.bind(shopOption16, "tears of trees"))
+	shopOption16.mouse_exited.connect(_on_mouse_exited.bind(shopOption16, "tears of trees"))
 	shopOption16.pressed.connect(_on_buyIngredient.bind("tears of trees"))
 	
 	$continueButton.mouse_entered.connect(_on_hovered.bind(true))
 	$continueButton.mouse_exited.connect(_on_hovered.bind(false))
 	
+func __init__(_allIngredients):
+	allIngredients = _allIngredients
 func _on_continueButton_pressed() -> void:
 	newDay.emit()
 
@@ -163,9 +169,15 @@ func checkIfTooBroke():
 		else:
 			lable.set("theme_override_colors/font_color", blackColor)
 
-func _on_mouse_entered(button: Variant):
+func _on_mouse_entered(button: Variant, ingred_name):
+	var ingred = allIngredients.get(ingred_name)
+	instance = spawn_test.instantiate()
+	add_child(instance)
+	instance.get_child(0).text = (ingred.itemName + "\nOwned: " + str(ingred.amountOwned))
 	button.material.set_shader_parameter("outline_thickness", 5.0)
 
 
-func _on_mouse_exited(button: Variant):
+func _on_mouse_exited(button: Variant, ingred_name):
+	if instance != null:
+		instance.queue_free()
 	button.material.set_shader_parameter("outline_thickness", 0.0)
