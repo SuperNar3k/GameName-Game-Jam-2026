955d1c6522b90c4d67917c511db924b4a9788d52
Fuzzy <luispelayo314@gmail.com>
2026-05-12 21:11:11 -0500

    fixed the hovering text from going off screen and also made it appear above the mouse

--- gngj/Scenes/spawn_test.gd
+++ gngj/Scenes/spawn_test.gd
@@ -3,5 +3,17 @@ extends Sprite2D
 
 
 func _process(_delta):
-	var mouse_pos = get_global_mouse_position()
-	global_position = mouse_pos
+	var x
+	var y
+	if(get_global_mouse_position().x + $Label.size.x > 1890):
+		x = 1890 - $Label.size.x
+	else: 
+		x = get_global_mouse_position().x
+		
+	if((get_global_mouse_position().y - 150) < 110):
+		y = 110
+	else: 
+		y = get_global_mouse_position().y - 150
+	
+	global_position = Vector2(x, y)
+	
