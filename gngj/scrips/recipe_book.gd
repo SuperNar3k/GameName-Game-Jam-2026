0352b332ccdd37af4cc00241ed134d9c76b86725
Carter-Layton <carter.layt@gmail.com>
2026-06-09 20:01:36 -0500

    removed a feature (requested by hr)
    also changed some layering with the scene transition

--- gngj/scrips/recipe_book.gd
+++ gngj/scrips/recipe_book.gd
@@ -49,6 +49,7 @@ func _create_button(pot_index) -> void:
 		if !DudFound:
 			content_button.text += "Duds :("
 			toc_container.add_child(content_button)
+			content_button.disabled = true
 			content_button.pressed.connect(_go_to_page.bind(pot_index))
 			DudFound = true
 			return
@@ -57,6 +58,7 @@ func _create_button(pot_index) -> void:
 
 	content_button.text += allPotions.get(allPotions.keys()[pot_index]).itemName
 	toc_container.add_child(content_button)
+	content_button.disabled = true
 	var i = 0
 	while i < toc_container.get_children().size():
 		if toc_container.get_child(i).text < content_button.text:
